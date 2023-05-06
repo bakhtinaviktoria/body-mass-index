4ce069463bb91e0a30b958946d4f543ee1e594a3
Viktoria B <bakhtina-vo@yandex.ru>
2023-05-06 18:34:20 +0300

    Main update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        int index = service.calculate(1.87, 98); // должно получиться 28
+        int index = service.calculate(1.67, 50); // должно получиться 17
         System.out.println(index);
     }
 }
